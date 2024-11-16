80ab1a91bf8b91920c29807c65739eaff4b24b39
Michael Koscheck <mkoscheck@stud.hs-heilbronn.de>
2024-11-16 23:42:35 +0900

    feat: refinement object detection

--- yolov8_detection/setup.py
+++ yolov8_detection/setup.py
@@ -4,7 +4,7 @@ package_name = 'yolov8_detection'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,11 +15,12 @@ setup(
     zip_safe=True,
     maintainer='mkoscheck',
     maintainer_email='mkoscheck@stud.hs-heilbronn.de',
-    description='TODO: Package description',
+    description='Object detection using YOLOv8 pretrained model',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'object_detection = yoloov8_detection.object_detection:main',
         ],
     },
 )
--- yolov8_detection/yolov8_detection/object_detection.py
+++ yolov8_detection/yolov8_detection/object_detection.py
@@ -2,16 +2,18 @@ import rclpy
 from rclpy.node import Node   
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from std_msgs.msg import Bool
 import cv2
 from ultralytics import YOLO
 
-class BackpackDetectionNode(Node):
+class ObjectDetectionNode(Node):
     def __init__(self, name): 
         super().__init__(name)
         self.model = YOLO('../models/yolov8n.pt')
         self.bridge = CvBridge()
-        self.image_sub = self.create_subscription(Image, "/camera/image_raw", self.image_callback, 10)
-        self.image_pub = self.create_publisher(Image, "/yolov8/annotated_image", 10)
+        self.image_sub = self.create_subscription(Image, "/camera/color/image_raw", self.image_callback, 1)
+        self.image_pub = self.create_publisher(Image, "/yolov8/annotated_image", 1)
+        self.buzzer_pub = self.create_publisher(Bool, "/buzzer", 10)
         self.BACKPACK_CLASS_INDEX = 24
 
     def image_callback(self, msg):
@@ -21,6 +23,14 @@ class BackpackDetectionNode(Node):
 
             # Perform YOLOv8 detection (filtering for "backpack")
             results = self.model.predict(frame, conf=0.5, classes=[self.BACKPACK_CLASS_INDEX])
+
+            if len(results[0].boxes) > 0:
+                print(f"Found {len(results)} backpacks")
+                # Publish buzzer signal
+                buzzer_msg = Bool()
+                buzzer_msg.data = True
+                self.buzzer_pub.publish(buzzer_msg)
+
             annotated_frame = results[0].plot()  # Annotate frame with detections
 
             # Display or process annotated frame
@@ -36,7 +46,7 @@ class BackpackDetectionNode(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = BackpackDetectionNode("backpack_detection_node")
+    node = ObjectDetectionNode("object_detection_node")
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
