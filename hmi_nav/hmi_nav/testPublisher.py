c880d5cca67ba29e68a2a8a7929268674ac2e224
Michael Koscheck <mkoscheck@stud.hs-heilbronn.de>
2024-11-17 11:01:09 +0900

    feat: first impl p2pnav and testPublisher

---
+++ hmi_nav/hmi_nav/testPublisher.py
@@ -0,0 +1,46 @@
+import rclpy
+from rclpy.node import Node
+from yahboomcar_msgs.msg import GoalData
+import yaml
+
+CONFIG_PATH = 'config.yaml'
+
+class TestPublisher(Node):
+    def __init__(self):
+        super().__init__('test_publisher')
+        self.pub_new_goal = self.create_publisher(GoalData, 'nav/new_goal', 1)
+
+        with open(CONFIG_PATH, 'r') as file:
+            configs = yaml.safe_load(file)
+
+        valid_input = False
+
+        while not valid_input:
+            print('Available configs:')
+            for config in configs['configs']:
+                self.get_logger().info(f" - {config}")
+
+            user_input = input("Enter the config name: ")
+            if user_input in configs['configs']:
+                self.config = configs['configs'][user_input]
+                self.get_logger().info(f"Loaded config '{user_input}'")
+                valid_input = True
+            else:
+                self.get_logger().error(f"Config '{user_input}' not found in config.yaml")
+
+        msg = GoalData()
+        msg.xData = self.config['xData']
+        msg.yData = self.config['yData']
+        msg.yaw = self.config['yaw']
+        self.pub_new_goal.publish(msg)
+        self.get_logger().info(f'Publishing: x={msg.xData}, y={msg.yData}, yaw={msg.yaw}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    test_publisher = TestPublisher()
+    rclpy.spin(test_publisher)
+    test_publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ p2pnav/p2pnav/send_goal.py
@@ -0,0 +1,52 @@
+import rclpy
+from rclpy.node import Node
+from nav2_msgs.action import NavigateToPose
+from geometry_msgs.msg import PoseStamped
+from rclpy.action import ActionClient
+from rclpy.task import Future
+from yahboomcar_msgs.msg import GoalData
+
+class GoalSender(Node):
+    def __init__(self):
+        super().__init__('goal_sender')
+        self.action_client = ActionClient(self, NavigateToPose, 'navigate_to_pose')
+        self.sub_newGoal = self.create_subscription(GoalData, "nav/new_goal", self.send_goal, 1)
+
+    def send_goal(self, msg):
+        if not isinstance(msg, GoalData): return
+
+        goal_msg = NavigateToPose.Goal()
+        goal_msg.pose.header.frame_id = 'map'
+        goal_msg.pose.header.stamp = self.get_clock().now().to_msg()
+
+        goal_msg.pose.pose.position.x = msg.xData
+        goal_msg.pose.pose.position.y = msg.yData
+        goal_msg.pose.pose.orientation.z = msg.yaw  # Use a quaternion here for real navigation
+
+        self.action_client.wait_for_server()
+        self._send_goal_future = self.action_client.send_goal_async(goal_msg)
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+        self.get_logger().info('Goal accepted :)')
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info(f'Goal completed with result: {result}')
+        self.destroy_node()
+        rclpy.shutdown()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    goal_node = GoalSender()
+    rclpy.spin(goal_node)
+
+if __name__ == '__main__':
+    main()
