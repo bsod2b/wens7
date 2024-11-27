b3d989f3d32dfd5be5f08c2e026fc13f06f7fe22
Michael Koscheck <mkoscheck@stud.hs-heilbronn.de>
2024-11-27 14:01:36 +0900

    fix: package dependencies

--- p2pnav/launch/navigation.launch.py
+++ p2pnav/launch/navigation.launch.py
@@ -1,9 +1,9 @@
+import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-import os
 
 def generate_launch_description():
     nav_package_launch_path =os.path.join(get_package_share_directory('yahboomcar_nav'), 'launch')
