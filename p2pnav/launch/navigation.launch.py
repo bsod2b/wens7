1b4391c49cf5d95c843190ac9de1f7d5c4ecc59f
Michael Koscheck <mkoscheck@stud.hs-heilbronn.de>
2024-11-27 13:42:11 +0900

    feat: navigation launch file

---
+++ p2pnav/launch/navigation.launch.py
@@ -0,0 +1,30 @@
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
+def generate_launch_description():
+    nav_package_launch_path =os.path.join(get_package_share_directory('yahboomcar_nav'), 'launch')
+
+    return LaunchDescription([
+        # Chassis bringup
+        Node(
+            package='yahboomcar_bringup',
+            executable='Ackman_driver_R2',
+        ),
+        # Lidar launch with Cartographer
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(
+            [nav_package_launch_path,'/map_cartographer_launch.py'])
+        ),
+        # TEB navigation
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(
+            [nav_package_launch_path,'/navigation_teb_launch.py'])
+        ),
+        # HMI listener and goal publisher
+        Node(
+            package='p2pnav',
+            executable='send_goal',
+        ),
+    ])
