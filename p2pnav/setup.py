afa96acefb3d98b48627fc809d736080c91b5384
Michael Koscheck <mkoscheck@stud.hs-heilbronn.de>
2024-11-27 14:45:19 +0900

    fix: try glob

--- p2pnav/setup.py
+++ p2pnav/setup.py
@@ -11,7 +11,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        (os.path.join('share',package_name,'launch'),glob(os.path.join('launch','*launch.py'))),
+        (os.path.join('share',package_name,'launch'),glob.glob(os.path.join('launch','*launch.py'))),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
