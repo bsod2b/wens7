a7b5120eed10cadbee1ebb2a2e07f739a4668274
Michael Koscheck <mkoscheck@stud.hs-heilbronn.de>
2024-11-27 14:50:25 +0900

    feat: add delay

--- p2pnav/launch/navigation.launch.py
+++ p2pnav/launch/navigation.launch.py
@@ -2,11 +2,11 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
-    nav_package_launch_path =os.path.join(get_package_share_directory('yahboomcar_nav'), 'launch')
+    nav_package_launch_path = os.path.join(get_package_share_directory('yahboomcar_nav'), 'launch')
 
     return LaunchDescription([
         # Chassis bringup
@@ -14,17 +14,32 @@ def generate_launch_description():
             package='yahboomcar_bringup',
             executable='Ackman_driver_R2',
         ),
-        # Lidar launch with Cartographer
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(
-            [nav_package_launch_path,'/map_cartographer_launch.py'])
+        # Delay before starting Lidar launch with Cartographer
+        TimerAction(
+            period=2.0,
+            actions=[
+                IncludeLaunchDescription(PythonLaunchDescriptionSource(
+                    [nav_package_launch_path, '/map_cartographer_launch.py'])
+                )
+            ]
         ),
-        # TEB navigation
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(
-            [nav_package_launch_path,'/navigation_teb_launch.py'])
+        # Delay before starting TEB navigation
+        TimerAction(
+            period=7.0,
+            actions=[
+                IncludeLaunchDescription(PythonLaunchDescriptionSource(
+                    [nav_package_launch_path, '/navigation_teb_launch.py'])
+                )
+            ]
         ),
-        # HMI listener and goal publisher
-        Node(
-            package='p2pnav',
-            executable='send_goal',
+        # Delay before starting HMI listener and goal publisher
+        TimerAction(
+            period=12.0,
+            actions=[
+                Node(
+                    package='p2pnav',
+                    executable='send_goal',
+                )
+            ]
         ),
     ])
