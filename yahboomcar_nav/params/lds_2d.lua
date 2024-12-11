03a47c43af356a79c7e7593c94990c1fe9e69bbf
Michael Koscheck <mkoscheck@stud.hs-heilbronn.de>
2024-12-11 14:20:47 +0900

    fix: published frame

--- yahboomcar_nav/params/lds_2d.lua
+++ yahboomcar_nav/params/lds_2d.lua
@@ -5,7 +5,6 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  publish_period_sec = 1.0,
   tracking_frame = "base_footprint",
   published_frame = "base_footprint",
   odom_frame = "odom",
