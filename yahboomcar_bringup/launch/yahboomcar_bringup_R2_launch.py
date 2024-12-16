de054a6027407a5d6f5e3178e1ca80e5281ff9c9
Michael Koscheck <mkoscheck@stud.hs-heilbronn.de>
2024-12-17 00:03:34 +0900

    fix: no second driver node

--- yahboomcar_bringup/launch/yahboomcar_bringup_R2_launch.py
+++ yahboomcar_bringup/launch/yahboomcar_bringup_R2_launch.py
@@ -103,7 +103,7 @@ def generate_launch_description():
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
         #rviz_node,
-        driver_node,
+        #driver_node,
         base_node,
         imu_filter_node,
         ekf_node,
