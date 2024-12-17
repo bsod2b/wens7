46b86f4b058e26918178d38f6c13ef9c82fb676d
Michael Koscheck <mkoscheck@stud.hs-heilbronn.de>
2024-12-17 09:10:53 +0900

    fix: import

--- yahboomcar_nav/launch/navigation_teb_launch.py
+++ yahboomcar_nav/launch/navigation_teb_launch.py
@@ -5,8 +5,7 @@ from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-from launch_ros.substitutions import PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 
 def generate_launch_description():
